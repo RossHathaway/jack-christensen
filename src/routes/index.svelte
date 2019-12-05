9bc5288ffd21c8d8c40c105f9aa56f6049ad2e8c
Ross Hathaway <hathaway.ross@gmail.com>
2019-12-04 23:31:25 -0500

    start of app including some work on homepage

---
+++ src/routes/index.svelte
@@ -0,0 +1,47 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+    font-style: italic;
+  }
+
+  photo-wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  img {
+    height: 100%;
+    max-width: 400px;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Jack Shields Christensen</title>
+</svelte:head>
+
+<h1>Jack Shields Christensen</h1>
+
+<div class="photo-wrapper">
+  <img alt='Jack in Hawaii' src='uncle-jack-in-hawaii.jpg'>
+  <img alt='Jack on diving tower' src='uncle-jack-on-diving-tower.jpg'>
+</div>
