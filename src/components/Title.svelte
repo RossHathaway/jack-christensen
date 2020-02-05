f0eeda75c9cd6caddb5750cb6ef5f96bd70ef565
Ross Hathaway <hathaway.ross@gmail.com>
2020-02-05 12:34:08 -0500

    update layout with basics from earlier commits that were lost when changing master totally.

---
+++ src/components/Title.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Logo from './Logo.svelte'
+  let logoSize = 200
+</script>
+
+<style>
+  header {
+    display: flex;
+  }
+  .title-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: var(--logoSize);
+  }
+  h1,
+  h2 {
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
+
+<header>
+  <Logo size={logoSize} />
+  <div class="title-container" style="--logoSize:{logoSize}">
+    <h1>Jack Shields Christensen</h1>
+    <h2>JackShieldsChristensen.com</h2>
+  </div>
+</header>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,31 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Nav from '../components/Nav.svelte';
+  import Title from '../components/Title.svelte'
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+  .container {
+    display: flex;
+    /* scroll ? */
+  }
 </style>
+<Title />
 
-<Nav {segment}/>
+<div class="container">
 
-<main>
-	<slot></slot>
-</main>
+  <Nav {segment} />
+
+  <main>
+    <slot></slot>
+  </main>
+
+</div>
