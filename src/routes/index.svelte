e68db32b52a0068d6fe88452be6a3113fd51829e
Ross Hathaway <hathaway.ross@gmail.com>
2020-02-05 17:56:23 -0500

    have homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,34 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
+<div>
+  <img alt="Jack in Hawaii" src="uncle-jack-in-hawaii.jpg" />
+  <img alt="Jack in Hawaii" src="uncle-jack-on-diving-tower.jpg" />
+</div>
 
 <p>
-  <strong
-    >Try editing this file (routes/index.html) to test hot module
-    reloading.</strong
-  >
+  Jack Shields Christensen, "Uncle Jack", suddenly passed away on May 7, 2018 at
+  the age of 87. For creative purposes over the years he was also legally known
+  as: Chris Christensen, Scott Burlington, and Reverend "Revi" Fiercely. He was
+  fully active to his last day.
+</p>
+<p>
+  This website is a compilation of Jack’s writings on his many life interests
+  and projects and is an offering to anyone who might share these interests.
+  Jack felt that everyone should take the time to explore their fascinations
+  fully and to share their life’s story. This statement by Erwin Schrodinger
+  inspired him to document his own reflections:
+</p>
+<p>
+  "If you cannot -- in the long run -- tell everyone what you have been doing,
+  your doing has been worthless."
+</p>
+<p>
+  *************************************************************************************
+  To communicate with the organizers of this website, please write to:
+</p>
+<p>
+  jscinfo@jackshieldschristensen.com
 </p>
 
 <style>
