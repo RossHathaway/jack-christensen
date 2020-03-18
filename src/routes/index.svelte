a2fad2891e8b51f8eb37ae44bfc8e56ff5c9b87d
Ross Hathaway <hathaway.ross@gmail.com>
2020-03-17 21:50:50 -0400

    remove mdsvex library because it was replaced in previous commit and also commented out unused styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,14 +36,14 @@
     justify-content: center;
   }
 
-  h1 {
+  /* h1 {
     text-align: center;
     margin: 0 auto;
     font-size: 2.8em;
     text-transform: uppercase;
     font-weight: 700;
     margin: 0 0 0.5em 0;
-  }
+  } */
 
   img {
     width: 100%;
@@ -62,8 +62,8 @@
   }
 
   @media (min-width: 480px) {
-    h1 {
+    /* h1 {
       font-size: 4em;
-    }
+    } */
   }
 </style>
