796647855eec480c69eecf04b5a17f1f733168f4
Ross Hathaway <hathaway.ross@gmail.com>
2020-05-02 20:49:31 -0400

    fix bug where trailing slash on url was not removed from fetch path in preload, causing 500 error

--- src/helpers/genericPreloadForLinks.js
+++ src/helpers/genericPreloadForLinks.js
@@ -1,10 +1,13 @@
+import { getLastPathSection } from './getLastPathSection';
+
 export async function genericPreloadForLinks(page, context) {
   try {
     const { path } = page;
-    const removedSlashes = path.split('/');
+    const trimmedPath = path.endsWith('/') ? path.slice(0, -1) : path;
+    const removedSlashes = trimmedPath.split('/');
     const lastPathSection = removedSlashes[removedSlashes.length - 1];
 
-    const res = await context.fetch(path + '.json');
+    const res = await context.fetch(trimmedPath + '.json');
     const links = await res.json();
 
     return { links, lastPathSection };
