c9bb2e9d619e7f26ee23fe20adf8cb355d119510
Ross Hathaway <hathaway.ross@gmail.com>
2020-05-26 21:04:51 -0400

    flesh out plan for copy plugin

--- src/helpers/rollup-plugin-copy-md-based-on-frontmatter.js
+++ src/helpers/rollup-plugin-copy-md-based-on-frontmatter.js
@@ -2,52 +2,76 @@ import fs from 'fs';
 
 export default function copy(options = {}) {
   const {
-    copyOnce = false,
-    flatten = true,
+    makeDestinationFromFrontMatter = makeDestFromFrontMatter,
     hook = 'buildEnd',
+
+    copyOnce = false,
     targets = [],
     verbose = false,
     ...restPluginOptions
   } = options;
 
-  // for each target, create read stream
-
-  targets.forEach(copyFiles);
+  targets.forEach(copyFile);
 }
 
-function copyFiles(target) {
+/*
+The moment that backpressure is triggered can be narrowed exactly to the return value of a Writable's .write() function. This return value is determined by a few conditions, of course.
+
+In any scenario where the data buffer has exceeded the highWaterMark or the write queue is currently busy, .write() will return false.
+
+When a false value is returned, the backpressure system kicks in. It will pause the incoming Readable stream from sending any data and wait until the consumer is ready again. Once the data buffer is emptied, a 'drain' event will be emitted and resume the incoming data flow.
+
+Once the queue is finished, backpressure will allow data to be sent again. The space in memory that was being used will free itself up and prepare for the next batch of data.
+*/
+function copyFile(target) {
   let isWritingToFile = false;
   let isWritingToFrontMatter = false;
   let frontMatter = '';
-  let writableStream;
+  let writeStream;
+  const readStream = fs.createReadStream(target);
 
-  fs.createReadStream(target);
-  // on data
-  if (!isWritingToFile && !isWritingToFrontMatter) {
-    // check for ---
-    //   if ---, isWritingToFrontMatter = true
-    //    frontMatter += buffer
-    // } else if {
-    //    //    no front matter; start writing file
-    //   isWritingToFile = true
-    //  }
-  } else if (isWritingToFile) {
-  } else if (!isWritingToFile && isWritingToFrontMatter) {
-    // if buffer contains ---
-    // finish adding to frontmatter
-    // isWritingToFrontMatter = false
-    // isWritingToFile = true
-    // pause read stream
-    const dest = getDestFromFrontMatter(frontMatter);
-    writableStream = fs.createWriteStream(dest);
-    // can I pipe this stream to writableStream?
-  }
+  readStream.on('readable', function handleChunkFromReadStream() {
+    const chunk = readStream.read();
 
-  // on pause of readStream
-  getDestFromFrontMatter(frontMatter);
+    if (isWritingToFile) {
+      writeStream.write(chunk);
+    } else {
+      // write to frontMatter
+      if (isWritingToFrontMatter) {
+        frontMatter += chunk;
+
+        if (chunk.lastIndexOf('---') > 2) {
+          // finished collecting front matter
+          const frontMatterSections = frontMatter.split('---');
+          const frontMatterWithoutBoundaries = frontMatterSections[1];
+          const startOfDocument = frontMatterSections[3];
+
+          isWritingToFile = true;
+          // pause read stream if async??????????????????
+          const dest = makeDestFromFrontMatter(frontMatterWithoutBoundaries);
+          writeStream = fs.createWriteStream(dest);
+          writeStream.write(startOfDocument);
+        }
+      } else {
+        // start of readstream;
+        const hasFrontMatterInChunk =
+          (chunk.length >= 3 && chunk.startsWith('---')) ||
+          chunk === '-' ||
+          chunk === '--';
+        if (hasFrontMatterInChunk) {
+          isWritingToFrontMatter = true;
+          frontMatter += chunk;
+        } else {
+          // no front matter; start writing file
+          isWritingToFile = true;
+          writeStream = fs.createWriteStream(/* need to accept target if we want to allow specified destinations */);
+        }
+      }
+    }
+  });
 }
 
-function getDestFromFrontMatter(frontMatter) {
+function makeDestFromFrontMatter(frontMatter) {
   let dest;
 
   return dest;
