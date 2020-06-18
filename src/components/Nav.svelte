5995cf0ba22e10069207e668d5e515796a1039b0
Ross Hathaway <hathaway.ross@gmail.com>
2020-06-17 21:41:24 -0400

    add colors to nav sections (need to remove colors applied to NavSection for new colors to be applied)

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,21 +3,25 @@
   import Logo from './Logo.svelte'
 
   export let logoSize, links
-  console.log('links:', links)
+
+  console.log('links', links)
+
 </script>
+
+<!-- {path: "about-uncle-jack", name: "About Uncle Jack", lastUrlSegment: "about-uncle-jack", children: Array(2)}
+1: {path: "categories", name: "Categories", lastUrlSegment: "categories", children: Array(17)}
+2: {path: "contact", name: "Contact", lastUrlSegment: "contact", children: null}
+3: {path: "featured-topics", name: "Featured Topics", lastUrlSegment: "featured-topics", children: Array(5)}
+4: {path: "index", name: "Index",  -->
 
 <div class="outer-container">
   <Logo size={logoSize} />
   <nav>
-    <!-- children: (2) [{…}, {…}]
-lastUrlSegment: "about-uncle-jack"
-name: "About Uncle Jack"
-path: "about-uncle-jack" -->
   {#each links as link}
     {#if link.name === "Index"}
     <!-- no index page -->
     {:else}
-      <div>
+      <div class="{link.path}">
       {#if link.name === "About Uncle Jack"}
         <h3>HOME</h3>
       {:else}
@@ -49,6 +53,26 @@ path: "about-uncle-jack" -->
 
   :global(nav a[aria-current]) {
     border-bottom: 2px solid white;
+  }
+
+  nav > div {
+    color: white;
+  }
+
+  nav > div.about-uncle-jack {
+    background-color: var(--second-darkest-hue);
+  }
+
+  nav > div.featured-topics {
+    background-color: var(--second-darkest-hue);
+  }
+
+  nav > div.categories {
+    background-color: var(--darkest-hue);
+  }
+
+  nav > div.contact {
+    background-color: var(--darkest-hue);
   }
 
 </style>
