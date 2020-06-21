28e70a142a4221b8fb625709af090cc33231df33
Ross Hathaway <hathaway.ross@gmail.com>
2020-06-20 21:05:10 -0400

    remove Sapper Project Template from head on one page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,3 @@
-<svelte:head>
-  <title>Sapper project template</title>
-</svelte:head>
-
-<div>
-  <img alt="Jack in Hawaii" src="uncle-jack-on-diving-tower.jpg" />
-</div>
-
-<p>
-  Jack Shields Christensen, "Uncle Jack", suddenly passed away on May 7, 2018 at
-  the age of 87. For creative purposes over the years he was also legally known
-  as: Chris Christensen, Scott Burlington, and Reverend "Revi" Fiercely. He was
-  fully active to his last day.
-</p>
-<p>
-  This website is a compilation of Jack’s writings on his many life interests
-  and projects and is an offering to anyone who might share these interests.
-  Jack felt that everyone should take the time to explore their fascinations
-  fully and to share their life’s story. This statement by Erwin Schrodinger
-  inspired him to document his own reflections:
-</p>
-<p class="border">
-  "If you cannot -- in the long run -- tell everyone what you have been doing,
-  your doing has been worthless."
-</p>
-<p>
-  To communicate with the organizers of this website, please write to:
-</p>
-<p>
-  jscinfo@jackshieldschristensen.com
-</p>
-
 <style>
   div {
     display: flex;
@@ -58,3 +26,27 @@
     } */
   }
 </style>
+
+<div>
+  <img alt="Jack in Hawaii" src="uncle-jack-on-diving-tower.jpg" />
+</div>
+
+<p>
+  Jack Shields Christensen, "Uncle Jack", suddenly passed away on May 7, 2018 at
+  the age of 87. For creative purposes over the years he was also legally known
+  as: Chris Christensen, Scott Burlington, and Reverend "Revi" Fiercely. He was
+  fully active to his last day.
+</p>
+<p>
+  This website is a compilation of Jack’s writings on his many life interests
+  and projects and is an offering to anyone who might share these interests.
+  Jack felt that everyone should take the time to explore their fascinations
+  fully and to share their life’s story. This statement by Erwin Schrodinger
+  inspired him to document his own reflections:
+</p>
+<p class="border">
+  "If you cannot -- in the long run -- tell everyone what you have been doing,
+  your doing has been worthless."
+</p>
+<p>To communicate with the organizers of this website, please write to:</p>
+<p>jscinfo@jackshieldschristensen.com</p>
