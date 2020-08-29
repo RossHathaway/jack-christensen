c93dce021c4d8a0a461cab15687095fafbb83be2
Ross Hathaway <hathaway.ross@gmail.com>
2020-08-29 18:05:48 -0400

    allow main title to appear larger when screen is medium/small by reducing space between text element and its svg container and adjusting media queries to prevent text from becoming too big when screen is wide.

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -18,20 +18,26 @@
 
   h1 > svg {
     width: clamp(300px, 800px, 800px);
-    max-width: 100%;
+    max-width: 600px;
   }
 
   svg > text {
     font-size: 50px;
     font-weight: bold;
   }
+
+  @media (max-width: 800px) {
+    h1 > svg {
+      max-width: 100%;
+    }
+  }
 </style>
 
 <header>
   <h1>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 180">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 180">
 
-      <text x="110" y="110">Jack Shields Christensen</text>
+      <text x="10" y="110">Jack Shields Christensen</text>
 
     </svg>
   </h1>
