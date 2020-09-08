0a382425d681686bfb3e815e438e75ebb81d3b79
Ross Hathaway <hathaway.ross@gmail.com>
2020-09-07 21:14:05 -0400

    add a way to specify section titles of the menu that should be different from their usual output of the file name processing function

--- src/helpers/makeReadableNameFromPath.js
+++ src/helpers/makeReadableNameFromPath.js
@@ -1,4 +1,11 @@
 export function makeReadableName(path) {
+  const namesToOverride = [{ 'obama-s-neighborhood': 'Obama\'s Neighborhood' }]
+  const indexOfNameToOverride = namesToOverride.findIndex((element) => element[path])
+
+  if (indexOfNameToOverride >= 0) {
+    return namesToOverride[indexOfNameToOverride][path]
+  }
+
   if (!path) return '';
   return path
     .split('-')
